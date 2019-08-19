6a47619d513a8236de3146d3af15a629e478ff29
taohuh <pimpakarn.wan@gmail.com>
2019-08-19 13:09:02 +0700

    Install HtmlWebpackPlugin and CleanWebpackPlugin

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
-    element.innerHTML = _.join(['Hello', 'webpack', 'สวัสดี'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
 
-    const myIcon = new Image();
-    myIcon.src = Icon;
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-    element.appendChild(myIcon);
+    element.appendChild(btn);
 
     return element;
 }
