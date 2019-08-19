b52ebb93cc4509b5e012a3186391aa8ee7d973b5
taohuh <pimpakarn.wan@gmail.com>
2019-08-19 16:12:44 +0700

    Install webpack-dev-server, middleware, HRM

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ import _ from 'lodash';
 import printMe from './print.js';
 
 function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
+	const element = document.createElement('div');
+	const btn = document.createElement('button');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.classList.add('hello');
 
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
+	btn.innerHTML = 'Click me and check the console!';
+	btn.onclick = printMe;
 
-    element.appendChild(btn);
+	element.appendChild(btn);
 
-    return element;
+	return element;
 }
 
 document.body.appendChild(component());
+
+if (module.hot) {
+	module.hot.accept('./print.js', function() {
+		console.log('Accepting the updated printMe module!');
+		printMe();
+	});
+}
